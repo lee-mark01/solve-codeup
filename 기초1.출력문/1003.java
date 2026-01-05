26a86e37e8fce2ad1158e8daf9aebe2ec33bc252
lee-mark01 <seungheelee1122@gmail.com>
2026-01-05 15:54:52 +0900

    20260105

--- 기초1.출력문/1003.java
+++ 기초1.출력문/1003.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
-        pw.println("Hello World");
+        pw.println("Hello");
+        pw.println("World");
         br.close();
         pw.close();
     }
 }
-
